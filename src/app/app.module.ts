63b61aec34b7ee9f11e513b954db58c3d7b684dd
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-07-11 17:36:44 -0300

    feat: create components header, main and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
-import { MoviesComponent } from './core/components/movies/movies.component';
-import { NavComponent } from './core/components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component'
+import { SpaceshipsComponent } from './pages/spaceships/spaceships.component';
+
+
+import { AngularMaterialModule } from './shared/angular-material/angular-material.module';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { MainComponent } from './core/components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MoviesComponent,
-    NavComponent
+    SpaceshipsComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
