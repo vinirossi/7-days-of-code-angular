d0b39fbf12354c38277daf4eac2fa0c45142ced6
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-07-11 17:36:59 -0300

    feat: set router of links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { SpaceshipsComponent } from './pages/spaceships/spaceships.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "movies",
+    component: MoviesComponent
+  },
+  {
+    path: "space-ships",
+    component: SpaceshipsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
