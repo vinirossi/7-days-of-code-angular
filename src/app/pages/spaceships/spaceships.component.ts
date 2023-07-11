5732923c10fd483a105ca93153c78bb4d905d577
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-07-11 17:37:41 -0300

    chore: change locale components of content

---
+++ src/app/pages/spaceships/spaceships.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spaceships',
+  templateUrl: './spaceships.component.html',
+  styleUrls: ['./spaceships.component.scss']
+})
+export class SpaceshipsComponent {
+
+}
